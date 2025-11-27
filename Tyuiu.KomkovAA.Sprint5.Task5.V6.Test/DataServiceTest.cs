1507a8c8a0e9aa52c072924ae985c81af13cf6ae
артур комков <stenplay33@gmail.com>
2025-11-28 01:28:33 +0500

    Task5

---
+++ Tyuiu.KomkovAA.Sprint5.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Tyuiu.KomkovAA.Sprint5.Task5.V6.Lib;
+namespace Tyuiu.KomkovAA.Sprint5.Task5.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Артур\Desktop\InPutDataFileTask5V6.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
---
+++ Tyuiu.KomkovAA.Sprint5.Task5.V6/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KomkovAA.Sprint5.Task5.V6.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = @"C:\Users\Артур\Desktop\InPutDataFileTask5V6.txt";
+        Console.WriteLine("Данные находятся в файле: " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine("Среднее значение всех вещественных = " + res);
+    }
+}
