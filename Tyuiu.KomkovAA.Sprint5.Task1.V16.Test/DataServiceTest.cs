223dbced8bbc40b937c9bc8428dd8167852bdac8
артур комков <stenplay33@gmail.com>
2025-11-27 20:07:32 +0500

    Task1

--- Tyuiu.KomkovAA.Sprint5.Task1.V16.Test/DataServiceTest.cs
+++ Tyuiu.KomkovAA.Sprint5.Task1.V16.Test/DataServiceTest.cs
@@ -1,6 +1,6 @@
 ﻿using System.IO;
 using Tyuiu.KomkovAA.Sprint5.Task1.V16.Lib;
-namespace Tyuiu.KomkovAA.Sprint5.Task1.V16.Task
+namespace Tyuiu.KomkovAA.Sprint5.Task1.V16.Test
 {
     [TestClass]
     public sealed class DataServiceTest
@@ -8,7 +8,7 @@ namespace Tyuiu.KomkovAA.Sprint5.Task1.V16.Task
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\Stud0\AppData\Local\Temp\OutPutFileTask1.txt";
+            string path = @"C:\Users\Артур\AppData\Local\Temp\OutPutFileTask1.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
