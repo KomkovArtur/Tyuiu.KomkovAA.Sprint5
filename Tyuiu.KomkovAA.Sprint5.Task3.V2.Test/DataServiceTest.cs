a41af0e358908797945b1b38337caf35308d0d4d
артур комков <stenplay33@gmail.com>
2025-11-27 21:46:17 +0500

    Task3

---
+++ Tyuiu.KomkovAA.Sprint5.Task3.V2.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Tyuiu.KomkovAA.Sprint5.Task3.V2;
+namespace Tyuiu.KomkovAA.Sprint5.Task3.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.txt");
+            FileInfo fileInfo = new FileInfo(path);
+            bool Exists = fileInfo.Exists;
+            Assert.IsTrue(Exists);
+        }
+    }
+}
---
+++ Tyuiu.KomkovAA.Sprint5.Task3.V2/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KomkovAA.Sprint5.Task3.V2;
+using Tyuiu.KomkovAA.Sprint5.Task3.V2.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds  = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int x = 3;
+        Console.WriteLine("x = " + x);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Файл: " + ds.SaveToFileTextData(x));
+        Console.WriteLine("Создан! ");
+    }
+}
