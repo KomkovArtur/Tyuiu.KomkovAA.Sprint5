4ccd42258a0ce2de06c4fa5f1aaf137bc885f4c5
артур комков <stenplay33@gmail.com>
2025-11-27 20:03:04 +0500

    Task1

---
+++ Tyuiu.KomkovAA.Sprint5.Task1.V16.Task/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Tyuiu.KomkovAA.Sprint5.Task1.V16.Lib;
+namespace Tyuiu.KomkovAA.Sprint5.Task1.V16.Task
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Stud0\AppData\Local\Temp\OutPutFileTask1.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KomkovAA.Sprint5.Task1.V16/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.KomkovAA.Sprint5.Task1.V16.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int start = -5, end = 5;
+        Console.WriteLine("Начало диапазона:" + start);
+        Console.WriteLine("Конец диапазона: " + end);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string res = ds.SaveToFileTextData(start, end);
+
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+
+    }
+}
