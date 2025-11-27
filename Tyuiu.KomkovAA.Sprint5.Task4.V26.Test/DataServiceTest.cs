1988e531e0a98a45d260643ee9777ae5ec3f607e
артур комков <stenplay33@gmail.com>
2025-11-27 22:58:52 +0500

    Task4

---
+++ Tyuiu.KomkovAA.Sprint5.Task4.V26.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Tyuiu.KomkovAA.Sprint5.Task4.V26.Lib;
+namespace Tyuiu.KomkovAA.Sprint5.Task4.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\Артур\Desktop\InPutDataFileTask4V26.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KomkovAA.Sprint5.Task4.V26/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KomkovAA.Sprint5.Task4.V26.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Исходные данные:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        string path = @"C:\Users\Артур\Desktop\InPutDataFileTask4V26.txt";
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(res);
+    }
+}
